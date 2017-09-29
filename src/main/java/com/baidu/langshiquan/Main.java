2a26a59a5f7d3c4e08454489a75db153a9c23350
langshiquan <langshiquan@baidu.com>
2017-09-29 11:32:22 +0800

    msg:init

---
+++ src/main/java/com/baidu/langshiquan/Main.java
@@ -0,0 +1,10 @@
+package com.baidu.langshiquan;
+
+/**
+ * Created by langshiquan on 17/9/29.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
